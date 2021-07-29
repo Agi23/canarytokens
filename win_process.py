3d23b588de42f73153cef3914b8eb71d551b5a0f
Agi <agoritsa.spirakis@gmail.com>
2021-07-29 17:43:51 +0200

    Cleaned up wid_process.py

--- win_process.py
+++ win_process.py
@@ -1,19 +1,11 @@
 import settings
-import tempfile
-import shutil
-import datetime
-import random
-from zipfile import ZipFile, ZipInfo
-from ziplib import MODE_DIRECTORY
 from cStringIO import StringIO
 
 MSI_TEMPLATE=settings.CANARY_MSI_TEMPLATE
 
-
 def make_canary_windows_process(template=MSI_TEMPLATE):
     with open(template, 'r') as f:
-        input_buf = StringIO(f.read())
-    output_buf = StringIO()
+        output_buf = StringIO(f.read())
     return output_buf.getvalue()
 
 if __name__ == '__main__':
